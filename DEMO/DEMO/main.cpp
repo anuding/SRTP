091302e44f0b9aa0f22f893ef98b494f9fc1d6fd
anuding <anuding@126.com>
2017-12-13 09:40:41 +0800

    加入config.xml 使用帧差法

--- DEMO/DEMO/main.cpp
+++ DEMO/DEMO/main.cpp
@@ -15,15 +15,21 @@ using namespace cv;
 
 void compareHist();
 int averagePic();
+void config();
+void concentration();
 
 int main() 
 {
+	
+	void concentration();
+
+
 	if (averagePic()==-1)//求解平均图
 		return 0;
 
 	Mat frame;
-	
-	CvCapture *capture = cvCreateFileCapture("E:/Code Project/SRTP/DEMO/DEMO/TEST00.mp4");
+
+	CvCapture *capture = cvCreateFileCapture("TEST00.mp4");
 	double totalframes=cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_COUNT);// 获取总帧数
 	
 	cvNamedWindow("原图", 0);
@@ -49,6 +55,103 @@ int main()
 
 	cvDestroyAllWindows();
 	return 0;
+}
+
+void config()
+{//E:/Code_Project/SRTP/CODES/DEMO/DEMO/
+	FileStorage fs("config.xml", FileStorage::READ);
+	bool flag = fs.isOpened();
+	cout << "flag = " << flag << endl << endl;
+	// if failed to open a file  
+	if (!fs.isOpened()) {
+		cout << "failed to open file test.yml " << endl << endl;
+		return ;
+	}
+	string videoPath;
+	string videoSuffix;
+	Rect roiRect;
+	string imgSavePath;
+
+	fs["videoReadPath"] >> videoPath;
+	fs["videoSuffix"] >> videoSuffix;
+	fs["imgSavePath"] >> imgSavePath;
+	fs["roi"] >> roiRect;
+
+	cout << videoPath << endl
+		<< videoSuffix << endl
+		<< imgSavePath << endl
+		<< roiRect << endl;
+}
+
+void concentration()
+{
+	FileStorage fs("config.xml", FileStorage::READ);
+	bool flag = fs.isOpened();
+	cout << "flag = " << flag << endl << endl;
+	// if failed to open a file  
+	if (!fs.isOpened()) {
+		cout << "failed to open file test.yml " << endl << endl;
+		return;
+	}
+	string videoPath;
+	string videoSuffix;
+	Rect roiRect;
+	string imgSavePath;
+
+	fs["videoReadPath"] >> videoPath;
+	fs["videoSuffix"] >> videoSuffix;
+	fs["imgSavePath"] >> imgSavePath;
+	fs["roi"] >> roiRect;
+	// 查找文件目录下的所有视频文件 
+	//vector<string> videoPathStr = FindAllFile((videoPath + videoSuffix).c_str(), true);
+	// 先读取一个视频文件，用于获取相关的参数 
+	VideoCapture capture("E:/Code_Project/SRTP/CODES/DEMO/DEMO/TEST00.mp4");
+	// 视频大小 
+	Size videoSize(capture.get(CV_CAP_PROP_FRAME_WIDTH), capture.get(CV_CAP_PROP_FRAME_HEIGHT));
+	// 创建一个视频写入对象 
+	VideoWriter writer("../result.avi", CV_FOURCC('M', 'J', 'P', 'G'), 25.0, videoSize);
+
+	for (auto videoName : "E:/Code_Project/SRTP/CODES/DEMO/DEMO/TEST00.mp4")
+	{
+		capture.open(videoName); // 读入路径下的视频
+
+		Mat preFrame;
+		bool stop(false);
+
+		double totleFrameNum = capture.get(CV_CAP_PROP_FRAME_COUNT); // 获取视频总帧数
+
+		for (int frameNum = 0; frameNum < totleFrameNum; frameNum++)
+		{
+			Mat imgSrc;
+			capture >> imgSrc; // 读一视频的一帧 
+			if (!imgSrc.data)
+				break;
+			Mat frame;
+			cvtColor(imgSrc, frame, CV_BGR2GRAY);
+			++frameNum;
+			if (frameNum == 1)
+			{
+				preFrame = frame;
+			}
+			Mat frameDif;
+			absdiff(frame, preFrame, frameDif); // 帧差法 
+			preFrame = frame;
+
+			threshold(frameDif, frameDif, 30, 255, THRESH_BINARY); // 二值化
+
+			Mat imgRoi = frameDif(roiRect);
+			double matArea = cv::sum(imgRoi)[0];  // 计算区域面积
+
+			if (matArea / (imgRoi.rows*imgRoi.cols) > 0.1) // 面积比例大于10% 
+			{
+				writer << frameDif;// 写入视频 
+				cout << "finished" << endl;
+			}
+		}
+	}
+	
+	capture.release();
+	writer.release();
 }
 
 void compareHist()
@@ -122,7 +225,7 @@ int averagePic()
 
 
 	int nframe = 20;//利用前nfram帧求解平均图
-	CvCapture *capture = cvCreateFileCapture("E:/Code Project/SRTP/DEMO/DEMO/TEST00.mp4");
+	CvCapture *capture = cvCreateFileCapture("E:/Code_Project/SRTP/CODES/DEMO/DEMO/TEST00.mp4");
 	if (NULL == capture)
 	{
 		printf("没有找到该视频！\n");
